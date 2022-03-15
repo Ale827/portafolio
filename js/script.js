5de132b333f16189a637fd7ddd5bc303585bd770
Ale827 <alejandrosistem@hotmail.com>
2022-03-15 01:13:33 -0300

    mi portafolio

---
+++ js/script.js
@@ -0,0 +1,24 @@
+//scroll-up
+
+document.getElementById("btn-up").addEventListener('click', scrollUp);
+
+
+function scrollUp(){
+    var currentScroll = document.documentElement.scrollTop;
+    if (currentScroll > 0){
+        window.scrollTo (0, 0);
+
+    }
+}
+
+buttonUp = document.getElementById('btn-up');
+
+window.onscroll = function(){
+    var scrol = document.documentElement.scrollTop;
+
+    if(scrol > 500){
+        buttonUp.style.transform = "scale(1)";
+    }else if(scrol <500){
+        buttonUp.style.transform = "scale(0)";
+    }
+}
